dd6815beda314f7b1cbf3a74554b2bcaecb358e7
DimaMukhin <dima9348@gmail.com>
2017-08-14 13:47:53 -0500

    implemented post index page action

---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+export const FETCH_POSTS = 'FETCH_POSTS';
+
+const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
+const API_KEY = '?key=766BC476F25AD68CDC5F4B66DABD4';
+
+export function fetchPosts() {
+  const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
+
+  return {
+    type: FETCH_POSTS,
+    payload: request
+  };
+}
