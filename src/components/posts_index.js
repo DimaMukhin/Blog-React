bee305f9ea881c97942354beeb7d007e9fcf301a
DimaMukhin <dima9348@gmail.com>
2017-08-15 15:06:57 -0500

    add links to individual posts and post_show component

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,6 +1,8 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+
 import { fetchPosts } from '../actions';
 
 class PostsIndex extends Component {
@@ -12,7 +14,9 @@ class PostsIndex extends Component {
     return _.map(this.props.posts, post => {
       return (
           <li className="list-group-item" key={post.id}>
-            {post.title}
+            <Link to={`/posts/${post.id}`}>
+              {post.title}
+            </Link>
           </li>
       );
     });
@@ -21,6 +25,11 @@ class PostsIndex extends Component {
   render() {
     return (
       <div>
+        <div className="text-right">
+          <Link className="btn btn-primary" to="/posts/new">
+            Add a Post
+          </Link>
+        </div>
         <h3>Posts</h3>
         <ul className="list-group">
           {this.renderPosts()}
